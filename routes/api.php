a0eefb990f72c08677e10435713f0bb5dacc85e8
Dandiyaniar17 <rdandiyaniar@gmail.com>
2023-12-14 23:38:40 +0700

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::prefix('product')->group(function () {
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::get('', [ProductController::class, 'index']);
+    Route::post('', [ProductController::class, 'store']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
+Route::prefix('category')->group(function () {
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::get('', [CategoryController::class, 'index']);
+    Route::post('', [CategoryController::class, 'store']);
+    Route::put('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'destroy']);
+});
+
